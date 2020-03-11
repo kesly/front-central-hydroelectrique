5da086e5b31c53e1ec437876d401516e3011526a
Nel BOUVIER <nel.bouvier@veniseverteniort.org>
2020-03-11 15:00:15 +0100

    Adding Store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import logo from './logo.svg';
 import './App.css';
+import rootReducer from './RootReducer';
+import Hydraulics from './Hydraulics';
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Hydraulics />
+      </div>
+    </Provider>
   );
 }
 
