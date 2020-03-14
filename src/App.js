33d9c527b22fad4065955cc631cfbe1cbf05192a
kesly gassant <kellorkeskes@hotmail.fr>
2020-03-14 14:14:18 +0100

    merge-resolve conflit

--- src/App.js
+++ src/App.js
@@ -9,17 +9,7 @@ import NavBar from './Components/NavBar';
 import ReduxTester from './Components/ReduxTester';
 
 import 'bootstrap/dist/css/bootstrap.css';
-
-const store = createStore(
-  rootReducer,
-  applyMiddleware(thunk)
-);
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import logo from './logo.svg';
 import './App.css';
-import rootReducer from './RootReducer';
 import Hydraulics from './Hydraulics';
 
 const store = createStore(
